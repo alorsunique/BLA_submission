d0ee918d9cd8f8c55ec22a87778da084bc448586
alorsunique <sounique7800@gmail.com>
2025-06-20 18:36:07 +0800

    2025 June 20

--- embed_test.py
+++ embed_test.py
@@ -1,7 +1,55 @@
 import ollama
+import pandas as pd
+import os
+from pathlib import Path
+import json
 
-embed = ollama.embeddings(model='nomic-embed-text:v1.5', prompt='The house is called')
+def line_chunk(data_df):
+    line_chunk_list = []
+    for index, row in data_df.iterrows():
+        chunk = f"{row['name']} is a {row['category']} in {row['location']}. This is the description of the {row['category']}: {row['description']}"
+        print(chunk)
 
-print(type(embed))
+        line_chunk_list.append(chunk)
 
-print(embed)
+    return line_chunk_list
+
+
+def get_embed(line_chunk_list):
+    embedding_list = []
+    for chunk in line_chunk_list:
+        embedding_values = ollama.embeddings(model='nomic-embed-text:v1.5', prompt=chunk)['embedding']
+
+        print(len(embedding_values))
+        embedding_list.append(embedding_values)
+
+    return(embedding_list)
+
+def store_embed_json(embedding_list):
+    json_embed_path = Path("embedding.json")
+
+    if json_embed_path.exists():
+        os.remove(json_embed_path)
+    
+    with open(json_embed_path, 'w') as embed_json:
+        json.dump(embedding_list,embed_json)
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("dataset.csv") 
+
+    print(df)
+
+    line_chunk_list = line_chunk(df)
+
+    embedding_list = get_embed(line_chunk_list)
+
+    store_embed_json(embedding_list)
+
+
+
+#embed = ollama.embeddings(model='nomic-embed-text:v1.5', prompt='The house is called')
+
+#print(type(embed))
+
+# print(embed)
